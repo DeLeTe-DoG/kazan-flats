6385678daa82c30ed60b283ee267d6ff06087c2f
DeLeTe <delete_dog@mail.ru>
2024-08-16 23:45:35 +0300

    animation added

--- src/components/header/HeadBlock.jsx
+++ src/components/header/HeadBlock.jsx
@@ -1,8 +1,25 @@
+import { motion } from "framer-motion"
+
+const textAnimation = {
+    hidden: {
+        y: -100,
+        opacity: 0
+    },
+    visible: {
+        y: 0,
+        opacity: 1
+    }
+}
+
 export default function() {
     return(
-        <div className="blocks headBlock">
-            <h1 className="headTitle anim">Посуточно <br /> Казань</h1>
+        <motion.div 
+            initial="hidden"
+            whileInView="visible"
+            className="blocks headBlock"
+        >
+            <motion.h1 variants={textAnimation} className="headTitle anim">Посуточно <br /> Казань</motion.h1>
             <h3 className="headSubTitle anim">Доступно  |  Удобно</h3>
-        </div>
+        </motion.div>
     )
 }
